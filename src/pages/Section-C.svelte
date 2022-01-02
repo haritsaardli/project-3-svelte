aa46938628760316a0ca2b88f57d3fccde68ca76
haritsaardli <haritsaardli@gmail.com>
2022-01-02 17:55:14 +0700

    finish

--- src/pages/Section-C.svelte
+++ src/pages/Section-C.svelte
@@ -3,27 +3,88 @@
         background-color: #020E35;
         color: white;
     }
+    h1{
+        size: 40px;
+    }
+    h2{
+        font-size: 30px;
+        line-height: 38px;
+    }
+    h3{
+        font-size: 15px;
+        letter-spacing: 3px;
+        line-height: 19px;
+    }
+    p{
+        font-size: 16px;
+        line-height: 20px;
+    }
+    button{
+        background: linear-gradient(270deg, #8C32F0 0%, #E73587 100%);
+        color: white;
+        border-radius: 0;
+        width: 140px;
+        height: 40px;
+        font-size: 12px;
+        line-height: 15.1px;
+    }
 </style>
 
 <div class="container-fluid background m-0 p-0">
-    <div class="container">
-        <div class="row">
+    <div class="container py-5">
+        <div class="row text-center">
             <div class="col">
                 <h1>Our Work</h1>
             </div>
         </div>
     
-        <div class="row">
-            <div class="col">
-                <h1>gambar</h1>
+        <div class="row mt-4">
+            <div class="col" style="background-color: #F09532;">
+                <img class="img-fluid mx-auto d-block" src="images/section-C/work-1.svg" alt="" srcset="">
             </div>
             <div class="col">
-                <h1>text</h1>
+                <h2>Brand Name</h2>
+                <h3>iOS | ANDROID | WEB DEVELOPMENT</h3>
+                <p class="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col text-right">
+                <h2>Brand Name</h2>
+                <h3>iOS | ANDROID | WEB DEVELOPMENT</h3>
+                <p class="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div class="col pt-5" style="background-color: #8C32F0;">
+                <img class="img-fluid mx-auto d-block" src="images/section-C/work-2.svg" alt="" srcset="">
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col py-5" style="background-color: #E73587;">
+                <img class="img-fluid mx-auto d-block" src="images/section-C/work-3.svg" alt="" srcset="">
+            </div>
+            <div class="col">
+                <h2>Brand Name</h2>
+                <h3>iOS | ANDROID | WEB DEVELOPMENT</h3>
+                <p class="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col text-right">
+                <h2>Brand Name</h2>
+                <h3>iOS | ANDROID | WEB DEVELOPMENT</h3>
+                <p class="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div class="col py-5" style="background-color: #32D6F0;">
+                <img class="img-fluid mx-auto d-block" src="images/section-C/work-4.svg" alt="" srcset="">
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col text-center">
+                <button class="btn">SEE ALL WORK</button>
             </div>
         </div>
     
     
-        <h1>Section C</h1>
     </div>
 
 </div>
--- src/pages/Section-D.svelte
+++ src/pages/Section-D.svelte
@@ -1,5 +1,67 @@
+<style>
+    h1{
+        font-size: 90px;
+        line-height: 110px;
+    }
+    h2{
+        font-size: 30px;
+        line-height: 38px;
+    }
+    h1,h2{
+        color: #020E35;
+    }
+    button{
+        background: linear-gradient(270deg, #8C32F0 0%, #E73587 100%);
+        font-size: 18px;
+        line-height: 13px;
+        height: 48px;
+        border-radius: 0;
+    }
+    .footer{
+        background-color: #020E35;
+        color: white;
+        font-size: 12px;
+        height: 25px;
+    }
+</style>
 
+<div class="container my-5">
+    <div class="row">
+        <div class="col">
+            <h2>Questions, Comments, Concerns?</h2>
+            <h1>We’d love to <span style="color: #E73587;">hear</span> from you!</h1>
+        </div>
+        <div class="col">
+            <form>
+                <div class="form-group">
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label>Your Email Address</label>
+                    <input type="email" class="form-control" placeholder="something@website.com">
+                </div>
+                <div class="form-group">
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label>Subject</label>
+                    <input type="text" class="form-control" placeholder="Question about your article">
+                </div>
+                <div class="form-group">
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label>Message</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Your message starts with…"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary btn-lg btn-block ">Send a Message</button>
+              </form>
+        </div>
+    </div>
+    <!-- <h1>Section D</h1> -->
+</div>
 
-<div>
-    <h1>Sectiona D</h1>
+<div class="container-fluid m-0 p-0 footer">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col">
+                <p class="my-auto">2020 © All rights reserved by Your company name</p>
+
+            </div>
+        </div>
+    </div>
 </div>
